037bad9fa35cf0ef3619a3fc597da896ee32407b
Tilen Juričan <tilen.jurican@gmail.com>
2025-02-17 16:20:34 +0100

    Refactor the code

--- main.py
+++ main.py
@@ -1,56 +1,60 @@
 import pickle
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
 
-MEM: dict[str, BeautifulSoup] = {}
-CLASSES: tuple[str, ...] = ('63279', '63218', '63283', '63208', '63213', '63738')
+MEM: dict[str, BeautifulSoup]
+SUBJECTS: set[str] = {'63220', '63216', '63280', '63217'}
+URL: str = 'https://urnik.fri.uni-lj.si/timetable/fri-2024_2025-letni'
+
+
+def load_mem():
+    global MEM
+
+    # noinspection PyBroadException
+    try:
+        with open('MEM.pickle', 'rb') as f:
+            MEM = pickle.load(f)
+    except Exception:
+        MEM = {}
+
+
+def store_mem():
+    with open('MEM.pickle', 'wb') as f:
+        pickle.dump(MEM, f)
 
 
 def get_soup_url(url: str, mem: bool = True) -> BeautifulSoup:
     if not mem or url not in MEM:
-        print(f'retrieving: {urlparse(url).netloc} ({url})\n')
+        print(f'retrieving: {urlparse(url).path} ({url})\n')
 
         session = requests.Session()
         page = session.get(url)
-        MEM[url] = BeautifulSoup(page.content, 'html.parser')
+        MEM[url] = BeautifulSoup(page.content, 'html.parser')  # 'html5lib')
     else:
-        print(f'cached: {urlparse(url).netloc} ({url})\n')
+        print(f'cached: {urlparse(url).path} ({url})\n')
 
     return MEM[url]
 
 
 def get_soup_file(file: str) -> BeautifulSoup:
     with open(file, 'r', encoding='utf-8') as _f:
-        return BeautifulSoup(_f.read(), 'html5lib')  # 'html.parser')
+        return BeautifulSoup(_f.read(), 'html.parser')  # 'html5lib')
 
 
-if __name__ == '__main__':
-    with open('mem', 'rb') as f:
-        MEM = pickle.load(f)
-
-    soup = get_soup_url(
-        # 'https://urnik.fri.uni-lj.si/timetable/fri-2024_2025-zimski/allocations?subject=63283&type=LV&type=AV&type=LAB'
-        # 'https://urnik.fri.uni-lj.si/timetable/fri-2024_2025-zimski/allocations?type=P&group=55603'
-        'https://urnik.fri.uni-lj.si/timetable/fri-2024_2025-zimski/allocations?group=55603'
-    )
-
-    with open('mem', 'wb') as f:
-        pickle.dump(MEM, f)
-
-    # soup = get_soup_file('Urnik.htm')
-
+def run(soup: BeautifulSoup) -> None:
     with open('original.html', 'w', encoding='utf-8') as f:
         f.write(soup.prettify())
 
     for div in soup.find_all('div', class_='bottom-aligned'):
         div.decompose()
 
-    for div in soup.find_all('div', class_='grid-entry'):
-        a = div.find('a', class_='link-subject')
-        if parse_qs(urlparse(a['href']).query)['subject'][0] not in CLASSES:
-            div.decompose()
+    # from urllib.parse import urlparse, parse_qs
+    # for div in soup.find_all('div', class_='grid-entry'):
+    #     a = div.find('a', class_='link-subject')
+    #     if parse_qs(urlparse(a['href']).query)['subject'][0] not in SUBJECTS:
+    #         div.decompose()
 
     for a in soup.find_all('a', class_='link-subject'):
         a.string = a.string.split('_')[0].split('(')[0]
@@ -61,10 +65,13 @@ if __name__ == '__main__':
     with open('modified.html', 'w', encoding='utf-8') as f:
         f.write(soup.prettify())
 
-# from selenium import webdriver
-#
-# browser = webdriver.Firefox()
-# browser.get(
-#     'https://urnik.fri.uni-lj.si/timetable/fri-2024_2025-zimski/allocations?subject=63283&type=LV&type=AV&type=LAB')
-#
-# print(browser.page_source)
+
+if __name__ == '__main__':
+    load_mem()
+
+    query: str = '/allocations?' + '&'.join('subject=' + subj for subj in sorted(SUBJECTS))
+    soup_ = get_soup_url(URL + query, True)
+
+    store_mem()
+
+    run(soup_)
